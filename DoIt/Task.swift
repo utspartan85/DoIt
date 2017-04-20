9219b9a7789f0dfd15f14f4bd5c5eddb1db195c6
Ari H Pineiro <utspartan85@yahoo.com>
2017-04-20 16:03:01 -0400

    Main page

---
+++ DoIt/Task.swift
@@ -0,0 +1,15 @@
+//
+//  Task.swift
+//  DoIt
+//
+//  Created by Ari H Pineiro on 4/20/17.
+//  Copyright © 2017 Ari H Pineiro. All rights reserved.
+//
+
+import Foundation
+
+class Task {
+    var name = ""
+    var important = false
+
+}
